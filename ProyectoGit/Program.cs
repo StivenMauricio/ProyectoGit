aee7236aaa746d8c382d361ed83ab43652e503fc
StivenMauricio <llenermauriciotorresurrutia@gmail.com>
2025-05-06 15:13:40 -0600

    jijija

--- ProyectoGit/Program.cs
+++ ProyectoGit/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("jaqueen peru");
+            Console.Writeline("Chupapi mi muneno");
              Console.Readkey();
         }
     }
