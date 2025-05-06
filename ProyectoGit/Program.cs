b5c6c6d9b8ae32caf87cc7e957e1ccff33740827
StivenMauricio <llenermauriciotorresurrutia@gmail.com>
2025-05-06 14:59:45 -0600

    agregado el metodo ReadKey

--- ProyectoGit/Program.cs
+++ ProyectoGit/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mi primer proyecto de GitHub");
+            Console.WriteLine("jaqueen peru");
+             Console.Readkey();
         }
     }
 }
